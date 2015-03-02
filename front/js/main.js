bf33560098cb1fd521ec3e74f052ff81520a2075
Eli Murray <ejmurra2@illinois.edu>
2015-03-01 23:19:53 -0600

    Initial commit

---
+++ front/js/main.js
@@ -0,0 +1,27 @@
+var people = [
+  {
+    title: 'test one',
+    tag: 'test tag one',
+    video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    clip: 'some sound'
+  },
+  {
+    title: 'test two',
+    tag: 'test tag two',
+    video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    clip: 'some sound'
+  },
+  {
+    title: 'test three',
+    tag: 'test tag three',
+    video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+    clip: 'some sound'
+  }
+]
+var render = function(people){
+  people.forEach(function(x){
+    $(".parallax").append('<div class="layer front">\n' + x.title + '</div>')
+  })
+}
+
+render(people)
